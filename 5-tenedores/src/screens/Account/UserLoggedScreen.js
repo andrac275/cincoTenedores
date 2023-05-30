cc7718b508a0f0005d2bcbe370bc0978a5ed63fd
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-05-30 12:33:34 +0200

    detectar si usuario esta loggeado o no en perfil

--- 5-tenedores/src/screens/Account/UserLoggedScreen.js
+++ 5-tenedores/src/screens/Account/UserLoggedScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export function AccountScreen() {
+export function UserLoggedScreen() {
   return (
     <View>
-      <Text>Estamos en la screen de AccountScreen</Text>
+      <Text>UserLoggedScreen</Text>
     </View>
   );
 }
