b6bba2a65c9a77546685406c39fc75f7bbb704a0
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-05-30 09:59:09 +0200

    menu bottom tab

--- 5-tenedores/src/screens/SearchScreen.js
+++ 5-tenedores/src/screens/SearchScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function SearchScreen() {
+export function SearchScreen() {
   return (
     <View>
       <Text>SearchScreen</Text>
