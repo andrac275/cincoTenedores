8f2c954354ff750d68e8453ca02cc834512bbaf4
Andreu Mut <andrac275@gmail.com>
2023-06-01 13:57:16 +0200

    55- controlando y validando datos del login

--- 5-tenedores/src/components/Auth/LoginForm/LoginForm.js
+++ 5-tenedores/src/components/Auth/LoginForm/LoginForm.js
@@ -2,17 +2,33 @@ import React, { useState } from "react";
 import { View } from "react-native";
 import { styles } from "./LoginForm.styles";
 import { Input, Icon, Button } from "react-native-elements";
+import { initialValues, validationSchema } from "./LoginForm.data";
+import { useFormik } from "formik";
 
 export function LoginForm() {
   const [hidePass, setVHidePass] = useState(true);
   const changePassVisibility = () => {
     setVHidePass((prev) => !prev);
   };
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: (formData) => {
+      console.log(formData.email);
+      console.log(formData.password);
+    },
+  });
+
   return (
     <View style={styles.content}>
       <Input
         placeholder="Correo electronico"
         containerStyle={styles.input}
+        onChangeText={(value) => {
+          formik.setFieldValue("email", value);
+        }}
+        errorMessage={formik.errors.email}
         rightIcon={
           <Icon type="material-community" name="at" iconStyle={styles.icon} />
         }
@@ -21,6 +37,10 @@ export function LoginForm() {
         placeholder="Contraseña"
         containerStyle={styles.input}
         secureTextEntry={hidePass}
+        onChangeText={(value) => {
+          formik.setFieldValue("password", value);
+        }}
+        errorMessage={formik.errors.password}
         rightIcon={
           <Icon
             type="material-community"
@@ -34,6 +54,8 @@ export function LoginForm() {
         title="Iniciar sesion"
         containerStyle={styles.btnContainer}
         buttonStyle={styles.btn}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}
       />
     </View>
   );
