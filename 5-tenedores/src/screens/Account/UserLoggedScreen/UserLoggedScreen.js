e72768b2586fa536666378487dde732721491630
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-06-02 09:32:44 +0200

    58_ Boton para cerrar sesion

--- 5-tenedores/src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
+++ 5-tenedores/src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Button } from "react-native-elements";
+import { getAuth, signOut } from "firebase/auth";
 import { InfoUser } from "../../../components/Account";
 import { styles } from "./UserLoggedScreen.styles";
 
 export function UserLoggedScreen() {
+  const logOut = async () => {
+    const auth = getAuth();
+    await signOut(auth);
+  };
   return (
     <View>
       <InfoUser />
+
+      <Button
+        title="Cerrar sesión"
+        buttonStyle={styles.btnStyle}
+        titleStyle={styles.btnTextStyle}
+        onPress={logOut}
+      />
     </View>
   );
 }
