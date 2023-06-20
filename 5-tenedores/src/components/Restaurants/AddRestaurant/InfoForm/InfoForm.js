ab6ee5d7b6185f90ead1b7127c0811730a1847f5
Andreu Mut <andrac275@gmail.com>
2023-06-20 17:15:43 +0200

    77 Controlando y validando formulario

--- 5-tenedores/src/components/Restaurants/AddRestaurant/InfoForm/InfoForm.js
+++ 5-tenedores/src/components/Restaurants/AddRestaurant/InfoForm/InfoForm.js
@@ -2,16 +2,48 @@ import React from "react";
 import { View } from "react-native";
 import { Input } from "react-native-elements";
 import { styles } from "./InfoForm.styles";
+import { ErrorMessage } from "formik";
 
-export function InfoForm() {
+export function InfoForm(props) {
+  const { formik } = props;
   return (
     <View style={styles.content}>
-      <Input placeholder="Nombre del restaurante" />
-      <Input placeholder="Dirección" />
+      <Input
+        placeholder="Nombre del restaurante"
+        onChangeText={(text) => {
+          formik.setFieldValue("name", text);
+        }}
+        errorMessage={formik.errors.name}
+      />
+      <Input
+        placeholder="Dirección"
+        onChangeText={(text) => {
+          formik.setFieldValue("address", text);
+        }}
+        errorMessage={formik.errors.address}
+      />
+      <Input
+        placeholder="Telefono"
+        onChangeText={(text) => {
+          formik.setFieldValue("phone", text);
+        }}
+        errorMessage={formik.errors.phone}
+      />
+      <Input
+        placeholder="Email"
+        onChangeText={(text) => {
+          formik.setFieldValue("email", text);
+        }}
+        errorMessage={formik.errors.email}
+      />
       <Input
         placeholder="Descripción del restaurante"
         multiline={true}
         inputContainerStyle={styles.textArea}
+        onChangeText={(text) => {
+          formik.setFieldValue("description", text);
+        }}
+        errorMessage={formik.errors.description}
       />
     </View>
   );
--- 5-tenedores/src/screens/Restaurants/AddRestaurantScreen/AddRestaurantScreen.js
+++ 5-tenedores/src/screens/Restaurants/AddRestaurantScreen/AddRestaurantScreen.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Button } from "react-native-elements";
 import { InfoForm } from "../../../components/Restaurants/AddRestaurant";
 import { styles } from "./AddRestaurantScreen.styles";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./AddRestaurantScreen.data";
 
 export function AddRestaurantScreen() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue);
+    },
+  });
   return (
     <View>
-      <InfoForm />
+      <InfoForm formik={formik} />
+      <Button
+        title="Crear restaurante"
+        buttonStyle={styles.addRestaurant}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}
+      />
     </View>
   );
 }
