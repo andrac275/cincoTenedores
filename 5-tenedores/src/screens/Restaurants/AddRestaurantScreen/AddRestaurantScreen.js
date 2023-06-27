c8be9b855e56f337417c5cfcafa25a88c4523cbe
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-06-27 16:21:30 +0200

    90 Creacion del restaurante

--- 5-tenedores/src/screens/Restaurants/AddRestaurantScreen/AddRestaurantScreen.js
+++ 5-tenedores/src/screens/Restaurants/AddRestaurantScreen/AddRestaurantScreen.js
@@ -8,15 +8,32 @@ import {
 } from "../../../components/Restaurants/AddRestaurant";
 import { styles } from "./AddRestaurantScreen.styles";
 import { useFormik } from "formik";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../../utils/firebase";
+import { useNavigation } from "@react-navigation/native";
+import { UUID } from "uuidjs";
 import { initialValues, validationSchema } from "./AddRestaurantScreen.data";
 
 export function AddRestaurantScreen() {
+  const navigation = useNavigation();
+
   const formik = useFormik({
     initialValues: initialValues(),
     validationSchema: validationSchema(),
     validateOnChange: false,
     onSubmit: async (formValue) => {
-      console.log(formValue);
+      try {
+        const newData = formValue;
+        newData.id = UUID.generate();
+        newData.createdAt = new Date();
+
+        const myDB = doc(db, "restaurants", newData.id);
+        await setDoc(myDB, newData);
+
+        navigation.goBack();
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
   return (
--- 5-tenedores/src/utils/firebase.js
+++ 5-tenedores/src/utils/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyANriOOJDZlc0PjPYVY0S-0SY7FOWkqUtE",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 export const initFirebase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(initFirebase);
