ef8fd31709f6a8afaf0ec82f3df801571d2f9ac8
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-06-28 10:05:32 +0200

    95 Obteniendo los datos del restaurante

--- 5-tenedores/src/screens/Restaurants/RestaurantInfoScreen/RestaurantInfoScreen.js
+++ 5-tenedores/src/screens/Restaurants/RestaurantInfoScreen/RestaurantInfoScreen.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { db } from "../../../utils";
+import {
+  doc,
+  onSnapshot,
+  query,
+  collection,
+  where,
+  orderBy,
+} from "firebase/firestore";
 import { styles } from "./RestaurantInfoScreen.styles";
 
 export function RestaurantInfoScreen(props) {
   const { route } = props;
-  console.log(route);
+  const [restaurant, setRestaurant] = useState(null);
+
+  useEffect(() => {
+    setRestaurant(null);
+    onSnapshot(doc(db, "restaurants", route.params.id), (doc) => {
+      setRestaurant(doc.data());
+    });
+  }, [route.params.id]);
+
   return (
     <View>
       <Text>RestaurantInformationScreen</Text>
