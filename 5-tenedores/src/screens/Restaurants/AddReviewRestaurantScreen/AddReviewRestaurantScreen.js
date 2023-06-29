b4532e64c021207fd67407b14d442d6cc0898d3b
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-06-29 13:48:36 +0200

    105 Formulario para anyadir review

--- 5-tenedores/src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
+++ 5-tenedores/src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
@@ -1,13 +1,38 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { AirbnbRating, Input, Button } from "react-native-elements";
 import { styles } from "./AddReviewRestaurantScreen.styles";
 
 export function AddReviewRestaurantScreen(props) {
   const { route } = props;
-  console.log(route.params);
+
   return (
-    <View>
-      <Text>AddReviewRestaurantScreen</Text>
+    <View style={styles.content}>
+      <View style={styles.ratingContent}>
+        <AirbnbRating
+          count={5}
+          reviews={["Pésimo", "Deficiente", "Normal", "Bueno", "Excelente"]}
+          defaultRating={0}
+          size={35}
+          onFinishRating={(rating) => {
+            console.log(rating);
+          }}
+        />
+      </View>
+      <View>
+        <Input placeholder="Titulo" />
+        <Input
+          placeholder="Comentario"
+          multiline
+          inputContainerStyle={styles.comment}
+        />
+      </View>
+
+      <Button
+        title="Enviar comentario"
+        containerStyle={styles.btnContainer}
+        buttonStyle={styles.btn}
+      />
     </View>
   );
 }
