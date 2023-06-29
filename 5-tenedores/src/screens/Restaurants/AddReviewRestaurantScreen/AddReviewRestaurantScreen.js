ba289efd0612869cbdabeb367e138fdbb493859f
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-06-29 14:03:05 +0200

    106 Validando formulario

--- 5-tenedores/src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
+++ 5-tenedores/src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { AirbnbRating, Input, Button } from "react-native-elements";
+import {
+  initialValues,
+  validationSchema,
+} from "./AddReviewRestaurantScreen.data";
+import { useFormik } from "formik";
 import { styles } from "./AddReviewRestaurantScreen.styles";
 
 export function AddReviewRestaurantScreen(props) {
   const { route } = props;
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue);
+    },
+  });
 
   return (
     <View style={styles.content}>
@@ -12,19 +25,27 @@ export function AddReviewRestaurantScreen(props) {
         <AirbnbRating
           count={5}
           reviews={["Pésimo", "Deficiente", "Normal", "Bueno", "Excelente"]}
-          defaultRating={0}
+          defaultRating={formik.values.rating}
           size={35}
           onFinishRating={(rating) => {
-            console.log(rating);
+            formik.setFieldValue("rating", rating);
           }}
         />
       </View>
       <View>
-        <Input placeholder="Titulo" />
+        <Input
+          placeholder="Titulo"
+          onChangeText={(text) => formik.setFieldValue("title", text)}
+          errorMessage={formik.errors.title}
+        />
         <Input
           placeholder="Comentario"
           multiline
           inputContainerStyle={styles.comment}
+          onChangeText={(text) => {
+            formik.setFieldValue("comment", text);
+          }}
+          errorMessage={formik.errors.comment}
         />
       </View>
 
@@ -32,6 +53,8 @@ export function AddReviewRestaurantScreen(props) {
         title="Enviar comentario"
         containerStyle={styles.btnContainer}
         buttonStyle={styles.btn}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}
       />
     </View>
   );
