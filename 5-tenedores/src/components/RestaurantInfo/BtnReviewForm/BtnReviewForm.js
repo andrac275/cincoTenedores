2b9613b72654becd3846142a0da99330d34fe54f
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-04 09:23:38 +0200

    110 Limitando una review por usuario

--- 5-tenedores/src/components/RestaurantInfo/BtnReviewForm/BtnReviewForm.js
+++ 5-tenedores/src/components/RestaurantInfo/BtnReviewForm/BtnReviewForm.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import { Text, Button } from "react-native-elements";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { query, collection, where, onSnapshot } from "firebase/firestore";
+import { size } from "lodash";
 import { useNavigation } from "@react-navigation/native";
-import { screen } from "../../../utils";
+import { screen, db } from "../../../utils";
 import { styles } from "./BtnReviewForm.styles";
 
 export function BtnReviewForm(props) {
   const { idRestaurant } = props;
   const [hasLogged, setHasLogged] = useState(false);
+  const [hasReview, setHasReview] = useState(false);
   const navigation = useNavigation();
   const auth = getAuth();
 
@@ -17,6 +20,22 @@ export function BtnReviewForm(props) {
       setHasLogged(user ? true : false);
     });
   }, []);
+
+  useEffect(() => {
+    if (hasLogged) {
+      const q = query(
+        collection(db, "reviews"),
+        where("idRestaurant", "==", idRestaurant),
+        where("idUser", "==", auth.currentUser.uid)
+      );
+
+      onSnapshot(q, (snapshot) => {
+        if (size(snapshot.docs) > 0) {
+          setHasReview(true);
+        }
+      });
+    }
+  }, [hasLogged]);
 
   const goToLogin = () => {
     navigation.navigate(screen.account.tab, { screen: screen.account.login });
@@ -27,6 +46,16 @@ export function BtnReviewForm(props) {
       idRestaurant,
     });
   };
+
+  if (hasLogged && hasReview) {
+    return (
+      <View style={styles.content}>
+        <Text style={styles.textSendReview}>
+          Ya has enviado un comentario a este restaurante.
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.content}>
