424a8f3457f75f1e70d2ae4919d374330cd6a012
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-07 13:13:41 +0200

    119 Screen favoritos usuario no loggeado

--- 5-tenedores/src/screens/FavoritesScreen.js
+++ 5-tenedores/src/screens/FavoritesScreen.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { UserNotLogged } from "../components/Favorites";
 
 export function FavoritesScreen() {
+  const [hasLogged, setHasLogged] = useState(null);
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setHasLogged(user ? true : false);
+    });
+  }, []);
+
+  if (!hasLogged) return <UserNotLogged />;
   return (
     <View>
       <Text>Estamos en la screen de FavoritesScreen</Text>
