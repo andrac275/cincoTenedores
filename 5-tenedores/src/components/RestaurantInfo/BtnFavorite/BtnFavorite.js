d3ada300bdfbe3122018fb33f897b62c12f1bf93
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-07 12:23:46 +0200

    116 Comprobando si el restaurante esta en favorito

--- 5-tenedores/src/components/RestaurantInfo/BtnFavorite/BtnFavorite.js
+++ 5-tenedores/src/components/RestaurantInfo/BtnFavorite/BtnFavorite.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import { Icon } from "react-native-elements";
 import { getAuth } from "firebase/auth";
@@ -13,11 +13,36 @@ import {
 } from "firebase/firestore";
 import { db } from "../../../utils";
 import { UUID } from "uuidjs";
+import { size } from "lodash";
 import { styles } from "./BtnFavorite.styles";
 
 export function BtnFavorite(props) {
   const { idRestaurant } = props;
   const auth = getAuth();
+  const [isFavorite, setIsFavorite] = useState(undefined);
+
+  useEffect(() => {
+    //Esto es una funcion anonima autoejecutable
+    (async () => {
+      const response = await getFavorites();
+      if (size(response)) {
+        setIsFavorite(true);
+      } else {
+        setIsFavorite(false);
+      }
+    })();
+  }, [idRestaurant]);
+
+  const getFavorites = async () => {
+    const q = query(
+      collection(db, "favorites"),
+      where("idRestaurant", "==", idRestaurant),
+      where("idUser", "==", auth.currentUser.uid)
+    );
+
+    const result = await getDocs(q);
+    return result.docs;
+  };
 
   const addFavorite = async () => {
     try {
@@ -34,15 +59,21 @@ export function BtnFavorite(props) {
     }
   };
 
+  const removeFavorite = async () => {
+    console.log("borrando favorito");
+  };
+
   return (
     <View style={styles.content}>
-      <Icon
-        type="material-community"
-        name="heart-outline"
-        color="#000"
-        size={35}
-        onPress={addFavorite}
-      />
+      {isFavorite !== undefined && (
+        <Icon
+          type="material-community"
+          name={isFavorite ? "heart" : "heart-outline"}
+          color={isFavorite ? "#f00" : "#000"}
+          size={35}
+          onPress={isFavorite ? removeFavorite : addFavorite}
+        />
+      )}
     </View>
   );
 }
