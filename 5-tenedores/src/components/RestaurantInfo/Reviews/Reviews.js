fed97402dab81f851fe60680d489b0f92825b45e
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-07 11:09:10 +0200

    112 pintando comentarios

--- 5-tenedores/src/components/RestaurantInfo/Reviews/Reviews.js
+++ 5-tenedores/src/components/RestaurantInfo/Reviews/Reviews.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Text, AirbnbRating, ListItem, Avatar } from "react-native-elements";
 import {
-  doc,
   onSnapshot,
   collection,
   query,
   where,
   orderBy,
 } from "firebase/firestore";
+import { map } from "lodash";
+import { Loading } from "../../Shared";
 import { db } from "../../../utils";
 import { styles } from "./Reviews.styles";
 
@@ -27,9 +29,37 @@ export function Reviews(props) {
     });
   }, []);
 
+  if (!reviews) {
+    return <Loading show text="Cargando..." />;
+  }
+
   return (
-    <View>
-      <Text>Reviews</Text>
+    <View style={styles.content}>
+      {map(reviews, (review) => {
+        const data = review.data();
+        const createdReview = new Date(data.createdAt.seconds * 1000);
+        return (
+          <ListItem key={data.id} bottomDivider containerStyle={styles.review}>
+            <Avatar source={{ uri: data.avatar }} size={50} rounded />
+            <ListItem.Content>
+              <ListItem.Title style={styles.title}>{data.title}</ListItem.Title>
+              <View style={styles.subtitle}>
+                <Text style={styles.comment}>{data.comment}</Text>
+                <View style={styles.contentRatingDate}>
+                  <AirbnbRating
+                    defaultRating={data.rating}
+                    showRating={false}
+                    size={15}
+                    isDisabled
+                    starContainerStyle={styles.starContainer}
+                  />
+                  <Text style={styles.date}>{String(createdReview)}</Text>
+                </View>
+              </View>
+            </ListItem.Content>
+          </ListItem>
+        );
+      })}
     </View>
   );
 }
