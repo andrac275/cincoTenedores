a49f785949e5586c8065e06d3c9af3132081dd61
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-07 13:26:40 +0200

    120 obteniendo lista de favoritos

--- 5-tenedores/src/screens/FavoritesScreen.js
+++ 5-tenedores/src/screens/FavoritesScreen.js
@@ -1,15 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  doc,
+  getDoc,
+  collection,
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 import { UserNotLogged } from "../components/Favorites";
+import { db } from "../utils";
 
 export function FavoritesScreen() {
   const [hasLogged, setHasLogged] = useState(null);
+  const [restaurants, setRestaurants] = useState(null);
   const auth = getAuth();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setHasLogged(user ? true : false);
+    });
+  }, []);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "favorites"),
+      where("idUser", "==", auth.currentUser.uid)
+    );
+
+    onSnapshot(q, async (snapshot) => {
+      let restaurantArray = [];
+      for await (const item of snapshot.docs) {
+        const data = item.data();
+        const docRef = doc(db, "restaurants", data.idRestaurant);
+        const docSnap = await getDoc(docRef);
+        const newData = docSnap.data();
+        newData.idFavorite = data.id;
+        restaurantArray.push(newData);
+      }
+
+      setRestaurants(restaurantArray);
     });
   }, []);
 
