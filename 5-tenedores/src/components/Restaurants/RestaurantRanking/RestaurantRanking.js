6dbd2673a6d9a3e34a7d998f6f1f1d4f2345bec7
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-10 09:06:03 +0200

    126 mostrando icono de ranking

--- 5-tenedores/src/components/Restaurants/RestaurantRanking/RestaurantRanking.js
+++ 5-tenedores/src/components/Restaurants/RestaurantRanking/RestaurantRanking.js
@@ -6,12 +6,36 @@ import { styles } from "./RestaurantRanking.styles";
 export function RestaurantRanking(props) {
   const { restaurant, index } = props;
 
+  const renderMedal = () => {
+    if (index > 2) return null; // que no muestre mas de 3 medallas. A partir de la 4a, no lo muestra
+    let color = "";
+    if (index === 0) {
+      color = "#FFD700"; //oro
+    } else if (index === 1) {
+      color = "#BEBEBE"; //plata
+    } else if (index === 2) {
+      color = "#CD7F32"; //bronbze
+    }
+
+    return (
+      <Icon
+        type="material-community"
+        name="medal-outline"
+        color={color}
+        style={styles.medal}
+      />
+    );
+  };
+
   return (
     <TouchableOpacity onPress={() => console.log("go to screen")}>
       <View style={styles.content}>
         <Image source={{ uri: restaurant.images[0] }} style={styles.image} />
         <View style={styles.infoContent}>
-          <Text style={styles.name}>{restaurant.name}</Text>
+          <View style={styles.nameContent}>
+            {renderMedal()}
+            <Text style={styles.name}>{restaurant.name}</Text>
+          </View>
           <Rating
             imageSize={15}
             readonly
