13e2388f63cf0dbbf7e7398de5e099436fb8fe63
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-10 08:33:25 +0200

    124 obteniendo todos los restaurantes por orden

--- 5-tenedores/src/screens/RankingScreen.js
+++ 5-tenedores/src/screens/RankingScreen.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  onSnapshot,
+} from "firebase/firestore";
+import { size } from "lodash";
+import { db } from "../utils";
 
 export function RankingScreen() {
+  const [restaurants, setRestaurants] = useState(null);
+  console.log(size(restaurants));
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "restaurants"),
+      orderBy("ratingMedia", "desc"),
+      limit(5)
+    );
+
+    onSnapshot(q, (snapshot) => {
+      setRestaurants(snapshot.docs);
+    });
+  }, []);
+
   return (
     <View>
-      <Text>Esta es la pantalla de RankingScreen</Text>
+      <Text>Esta2 es la pantalla de RankingScreen</Text>
     </View>
   );
 }
