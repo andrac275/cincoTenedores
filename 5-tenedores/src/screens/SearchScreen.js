8d7b0caaf4ecc9aec0d3cf3fd0d1f26cb241118a
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-12 14:36:47 +0200

    129 Buscando restaurantes

--- 5-tenedores/src/screens/SearchScreen.js
+++ 5-tenedores/src/screens/SearchScreen.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { View, ScrollView } from "react-native";
 import { Text, SearchBar, ListItem, Avatar, Icon } from "react-native-elements";
+import {
+  query,
+  collection,
+  getDocs,
+  startAt,
+  endAt,
+  limit,
+  orderBy,
+} from "firebase/firestore";
+import { db } from "../utils/";
 import { Loading } from "../components/Shared";
 
 export function SearchScreen() {
   const [searchResults, setSearchResults] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const q = query(
+        collection(db, "restaurants"),
+        orderBy("name"),
+        startAt(searchText),
+        endAt(`${searchText}\uf8ff`),
+        limit(20)
+      );
+
+      const querySnapshot = await getDocs(q);
+      setSearchResults(querySnapshot.docs);
+    })();
+  }, [searchText]);
 
   return (
     <>
