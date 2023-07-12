56e79a6afe2289b596350d4b68c7556a3c6bb67f
Andreu Mut Portes <andreu.mutportes@soprasteria.com>
2023-07-12 14:25:28 +0200

    128 Creando search bar

--- 5-tenedores/src/screens/SearchScreen.js
+++ 5-tenedores/src/screens/SearchScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, ScrollView } from "react-native";
+import { Text, SearchBar, ListItem, Avatar, Icon } from "react-native-elements";
+import { Loading } from "../components/Shared";
 
 export function SearchScreen() {
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <>
+      <SearchBar
+        placeholder="Busca tu restaurante"
+        value={searchText}
+        onChangeText={(text) => setSearchText(text)}
+      />
+
+      {!searchResults && <Loading show text="Cargando..." />}
+    </>
   );
 }
